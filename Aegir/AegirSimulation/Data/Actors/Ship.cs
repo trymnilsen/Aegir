2e0b9535acc2edf04e2d54489664f3076a37886a
Trym Nilsen <trym.nilsen@gmail.com>
2015-04-23 21:48:22 +0200

    Read only tests

--- Aegir/AegirSimulation/Data/Actors/Ship.cs
+++ Aegir/AegirSimulation/Data/Actors/Ship.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AegirLib.Data.Actors
 {
+    [ReadOnly(true)]
     public class Ship : Actor
     {
         public float Width { get; set; }
