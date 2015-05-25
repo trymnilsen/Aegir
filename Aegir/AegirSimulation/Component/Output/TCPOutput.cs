3dbf657559500b4bbe56bf4d6b73bcf68839a61a
Trym Nilsen <trym.nilsen@gmail.com>
2015-05-25 20:25:24 +0200

    component wip

---
+++ Aegir/AegirSimulation/Component/Output/TCPOutput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AegirLib.Component.Output
+{
+    public class TCPOutput : Component
+    {
+        public int Port { get; set; }
+        public string IpAddress { get; set; }
+
+        //Sentences
+        public bool outputGGA { get; set; }
+        public bool outputGLL { get; set; }
+    }
+}
