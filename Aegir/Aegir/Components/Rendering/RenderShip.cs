5de1600ff450dc170268eba185276985d0f633a0
Trym Nilsen <trym.nilsen@gmail.com>
2015-05-26 22:55:16 +0200

    Rendering Components

---
+++ Aegir/Aegir/Components/Rendering/RenderShip.cs
@@ -0,0 +1,28 @@
+﻿using Aegir.Rendering.Geometry.Buffer;
+using AegirLib.Component;
+using AegirLib.Simulation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aegir.Components.Rendering
+{
+    class RenderShip:Component
+    {
+        private VertexBuffer vertexData; 
+        public RenderShip()
+        {
+
+        }
+        public override void Update(DeltaTime delta)
+        {
+            base.Update(delta);
+        }
+        public override void Render(DeltaTime delta)
+        {
+            base.Render(delta);
+        }
+    }
+}
---
+++ Aegir/AegirSimulation/IO/Save/SaveFile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AegirLib.IO.Save
+{
+    public class SaveFile
+    {
+        private string filePath;
+        public SaveFile(string filePath)
+        {
+            this.filePath = filePath;
+        }
+        public void Load(string[] componentOrigins)
+        {
+
+        }
+        public void Save()
+        {
+
+        }
+    }
+}
