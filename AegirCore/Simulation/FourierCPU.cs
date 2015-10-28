eb9632ccc4426d442ae02c82576bd2ad583200ff
Trym Nilsen <trym.nilsen@gmail.com>
2015-10-28 20:36:04 +0100

    Ocean sim wip

--- AegirCore/Simulation/FourierCPU.cs
+++ AegirCore/Simulation/FourierCPU.cs
@@ -161,8 +161,8 @@ namespace AegirCore.Simulation
 
                         X = (int)m_butterflyLookupTable[bftIdx + 0];
                         Y = (int)m_butterflyLookupTable[bftIdx + 1];
-                        w.x = m_butterflyLookupTable[bftIdx + 2];
-                        w.y = m_butterflyLookupTable[bftIdx + 3];
+                        w.X = m_butterflyLookupTable[bftIdx + 2];
+                        w.Y = m_butterflyLookupTable[bftIdx + 3];
 
                         data0[idx, x + y * m_size] = FFT(w, data0[idx1, x + X * m_size], data0[idx1, x + Y * m_size]);
                         data1[idx, x + y * m_size] = FFT(w, data1[idx1, x + X * m_size], data1[idx1, x + Y * m_size]);
