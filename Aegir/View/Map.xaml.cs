014f702a8609b9a48e74e3b9e5805ced21e5bebd
Trym Nilsen <trym.nilsen@gmail.com>
2015-10-28 19:54:40 +0100

    Add Tabcontainer for Map/Vessel Dynamics

--- Aegir/View/Map.xaml.cs
+++ Aegir/View/Map.xaml.cs
@@ -32,15 +32,15 @@ namespace Aegir.View
         {
             if (!DesignerProperties.GetIsInDesignMode(this))
             {
-                mapControl.MapProvider = OpenSeaMapHybridProvider.Instance;
+                mapControl.MapProvider = GoogleMapProvider.Instance;
                 mapControl.Manager.Mode = GMap.NET.AccessMode.ServerAndCache;
-
+                
                 GMapProvider.WebProxy = null;
-                mapControl.Position = new PointLatLng(55.755786121111, 37.617633343333);
+                mapControl.Position = new PointLatLng(59.755786121111, 11);
 
                 mapControl.MinZoom = 1;
                 mapControl.MaxZoom = 20;
-                mapControl.Zoom = 10;
+                mapControl.Zoom = 8;
 
                 mapControl.DragButton = MouseButton.Left;
             }
