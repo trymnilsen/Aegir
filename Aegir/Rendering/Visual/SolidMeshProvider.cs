e64025979ae5bc22b1b6b9f88408bf4a7150ff19
Trym Nilsen <trym.nilsen@gmail.com>
2015-12-15 21:34:40 +0100

    Added solid mesh and visual cache skeleton

---
+++ Aegir/Rendering/Visual/SolidMeshProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+using Aegir.ViewModel.NodeProxy;
+
+namespace Aegir.Rendering.Visual
+{
+    public class SolidMeshProvider : IVisualProvider
+    {
+        public Visual3D GetVisual(NodeViewModelProxy node)
+        {
+            return null;
+        }
+    }
+}
