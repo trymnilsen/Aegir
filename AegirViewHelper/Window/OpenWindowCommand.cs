494e94d2ead35eab7a1caed6ca0ca527643dee79
Trym Nilsen <trym.nilsen@gmail.com>
2016-03-18 22:44:52 +0100

    Added Viewhelpers lib

---
+++ AegirViewHelper/Window/OpenWindowCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AegirViewHelper
+{
+    public class OpenWindowCommand : ICommand
+    {
+        public IWindowHandler Handler { get; set; }
+
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
