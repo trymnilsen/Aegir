2ff47570a266fa5790df6e4285aa04124974c695
Trym Nilsen <trym.nilsen@gmail.com>
2016-05-01 14:33:40 +0200

    Scope stopewatch util

--- Aegir/Util/DebugUtil.cs
+++ Aegir/Util/DebugUtil.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using log4net;
+using System;
+using System.Diagnostics;
 using System.IO;
 using System.Runtime.CompilerServices;
 
@@ -6,6 +8,7 @@ namespace Aegir.Util
 {
     public class DebugUtil
     {
+        private static readonly ILog defaultLog = LogManager.GetLogger(typeof(DebugUtil));
         public static void LogWithLocation(string logData,
             bool shortenCallerFilepath = true,
             [CallerMemberName] string memberName = "",
@@ -18,6 +21,34 @@ namespace Aegir.Util
                 sourceFilePath = fileInfo.Name;
             }
             Debug.WriteLine("[" + sourceFilePath + ":" + sourceLineNumber + "@" + memberName + "]" + logData);
+        }
+
+        public static IDisposable StartScopeWatch(string scopeDescription, ILog log = null)
+        {
+            if(log!=null)
+            {
+                log = defaultLog;
+            }
+            return new ScopeStopwatch(scopeDescription, log);
+        }
+        
+    }
+    public class ScopeStopwatch : IDisposable
+    {
+        private ILog log;
+        private Stopwatch stopwatch;
+        private string description;
+
+        public ScopeStopwatch(string description, ILog log)
+        {
+            this.log = log;
+            this.description = description;
+            stopwatch = Stopwatch.StartNew();
+        }
+        public void Dispose()
+        {
+            stopwatch.Stop();
+            log.DebugFormat("[ {0} ] used {1} ms", description, stopwatch.Elapsed.TotalMilliseconds);
         }
     }
 }
