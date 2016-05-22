abb23c561b5a0604d8f88c1d45676913be56afe0
Trym Nilsen <trym.nilsen@gmail.com>
2016-05-22 17:48:35 +0200

    Viewport gizmos

---
+++ Aegir/View/Rendering/ManipulatorGizmo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HelixToolkit;
+using HelixToolkit.Wpf;
+using System.Windows.Media;
+
+namespace Aegir.View.Rendering
+{
+    public class TransformTarget
+    {
+        public MatrixTransform Transform { get; set; }
+    }
+    public class ManipulatorGizmo
+    {
+        private TranslateManipulator translationManipulator;
+        private RotateManipulator rotationManipulator;
+        private TransformTarget target;
+        public ManipulatorGizmo()
+        {
+            target = new TransformTarget();
+            target.Transform.Changed += Transform_Changed;
+        }
+        
+
+
+        private void Transform_Changed(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Transform Changed");
+        }
+    }
+}
