9483b706cbaf7a56a445373e431de39889b1d6a8
Trym Nilsen <trym.nilsen@gmail.com>
2016-05-28 23:23:51 +0200

    scene click handler

---
+++ Aegir/View/Rendering/SceneClickHandler.cs
@@ -0,0 +1,22 @@
+﻿using Aegir.Rendering;
+using HelixToolkit.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aegir.View.Rendering
+{
+    public class SceneClickHandler
+    {
+        private HelixViewport3D[] viewports;
+        public IVisualResolver RenderingResolver { get; private set; }
+
+        public SceneClickHandler(IVisualResolver resolver, HelixViewport3D[] viewports)
+        {
+            RenderingResolver = resolver;
+            this.viewports = viewports;
+        }
+    }
+}
