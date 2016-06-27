4b0451e608ecfe6ca59c692ea0151225b7601431
Trym Nilsen <trym.nilsen@gmail.com>
2016-06-27 22:47:12 +0200

    Added XmlSerializing helpers

---
+++ AegirCore/Persistence/PersistanceException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AegirCore.Persistence
+{
+    public class PersistanceException : Exception
+    {
+        public PersistanceException(string message):
+            base(message) { }
+    }
+}
---
+++ AegirCore/Persistence/XElementSerializer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace AegirCore.Persistence
+{
+    public static class XElementSerializer
+    {
+        public static XElement SerializeToXElement(object obj)
+        {
+            throw new NotImplementedException();
+        }
+        public static T DeserializeFromXElement<T>(XElement element)
+        {
+            throw new NotImplementedException();
+        }
+        public static void AddElement(this XElement parent, string name, object value)
+        {
+            XElement element = new XElement(name);
+            element.SetValue(value);
+            parent.Add(element);
+        }
+        public static T GetElementAs<T>(this XElement element, string name)
+        {
+            XElement namedElement = element.Element(name);
+            if(namedElement == null)
+            {
+                throw new PersistanceException($"Expected element {name} not found under parent element {element.Name}");
+            }
+            return (T)Convert.ChangeType(namedElement.Value, typeof(T));
+        }
+    }
+}
