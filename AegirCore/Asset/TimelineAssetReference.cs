b3e75bbb0460bd3181839c24f676271df68e0b44
Trym Nilsen <trym.nilsen@gmail.com>
2016-07-03 22:33:28 +0200

    asset reference skeletons

--- AegirCore/Asset/TimelineAssetReference.cs
+++ AegirCore/Asset/TimelineAssetReference.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace AegirCore.Asset
 {
-    public class AssetReference<T> 
+    class TimelineAssetReference
     {
-
     }
 }
