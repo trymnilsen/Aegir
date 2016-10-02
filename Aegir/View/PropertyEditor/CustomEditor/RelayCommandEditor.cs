2729f95a9ef56a768890216a219fc90a68f2eed0
Trym Nilsen <trym.nilsen@gmail.com>
2016-10-02 16:34:35 +0200

    Removed extended toolkit

--- Aegir/View/PropertyEditor/CustomEditor/RelayCommandEditor.cs
+++ Aegir/View/PropertyEditor/CustomEditor/RelayCommandEditor.cs
@@ -1,22 +1,21 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using Xceed.Wpf.Toolkit.PropertyGrid;
-using Xceed.Wpf.Toolkit.PropertyGrid.Editors;
+
 
 namespace Aegir.View.PropertyEditor.CustomEditor
 {
-    public class RelayCommandEditor : ITypeEditor
-    {
-        public FrameworkElement ResolveEditor(PropertyItem propertyItem)
-        {
-            Button commandButton = new Button();
+    //public class RelayCommandEditor : ITypeEditor
+    //{
+    //    public FrameworkElement ResolveEditor(PropertyItem propertyItem)
+    //    {
+    //        Button commandButton = new Button();
 
-            Binding commandBinding = new Binding("Value");
-            commandButton.Content = new TextBlock() { Text = "Click" };
-            commandBinding.Source = propertyItem;
-            BindingOperations.SetBinding(commandButton, Button.CommandProperty, commandBinding);
-            return commandButton;
-        }
-    }
+    //        Binding commandBinding = new Binding("Value");
+    //        commandButton.Content = new TextBlock() { Text = "Click" };
+    //        commandBinding.Source = propertyItem;
+    //        BindingOperations.SetBinding(commandButton, Button.CommandProperty, commandBinding);
+    //        return commandButton;
+    //    }
+    //}
 }
