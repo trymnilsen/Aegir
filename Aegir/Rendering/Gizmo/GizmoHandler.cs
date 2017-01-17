b556ac6c3828d3badb688e0834d66b26c3771288
Trym Nilsen <trym.nilsen@gmail.com>
2017-01-17 22:05:05 +0100

    gizmos wip

---
+++ Aegir/Rendering/Gizmo/GizmoHandler.cs
@@ -0,0 +1,40 @@
+﻿using AegirLib.Behaviour.World;
+using System.Collections.Generic;
+
+namespace Aegir.Rendering
+{
+    public class GizmoHandler
+    {
+        public enum ViewportLayer
+        {
+            Overlay,
+            Scene
+        }
+
+        private List<IGizmo> activeSceneGizmos;
+        private bool visibility;
+
+        public bool Visibility
+        {
+            get { return visibility; }
+            set
+            {
+                if(value!=visibility)
+                {
+                    visibility = value;
+                    GizmoVisibilityChanged?.Invoke(this, value);
+                }
+            }
+        }
+
+        public delegate void GizmoVisibilityChangedHandler(GizmoHandler sender, bool visibility);
+        public event GizmoVisibilityChangedHandler GizmoVisibilityChanged;
+
+        public static void AddGizmo(IGizmo gizmo, ViewportLayer layer)
+        {
+            GizmoAdded?.Invoke(gizmo);
+        }
+        private delegate void GizmoAddedHandler(IGizmo gizmo);
+        private static event GizmoAddedHandler GizmoAdded; 
+    }
+}
---
+++ Aegir/Rendering/Gizmo/IGizmo.cs
@@ -0,0 +1,17 @@
+﻿using AegirLib.MathType;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aegir.Rendering
+{
+    public interface IGizmo
+    {
+        Vector3 Position { get; }
+        Quaternion Rotation { get; }
+
+
+    }
+}
