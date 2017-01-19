2aae5cdbe9aaae47967e3721d7c5f2718fe6d2ef
Trym Nilsen <trym.nilsen@gmail.com>
2017-01-19 22:32:50 +0100

    Treeview templates

---
+++ Aegir/View/Scenegraph/ScenegraphTreeTemplateSelector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Aegir.View.Scenegraph
+{
+    public class ScenegraphTreeTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate WorldTemplate { get; set; }
+        public DataTemplate EntityTemplate { get; set; }
+        public DataTemplate StaticTemplate { get; set; }
+        public DataTemplate TimelineTemplate { get; set; }
+        public DataTemplate GizmoTemplate { get; set; }
+        public DataTemplate KeyframeTemplate { get; set; }
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
