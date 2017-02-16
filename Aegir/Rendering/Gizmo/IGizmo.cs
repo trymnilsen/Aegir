b7426d1d9290ce19226347a5cc31e6215244c8d6
Trym Nilsen <trym.nilsen@gmail.com>
2017-02-16 22:06:59 +0100

    Add Translate gizmos files

--- Aegir/Rendering/Gizmo/IGizmo.cs
+++ Aegir/Rendering/Gizmo/IGizmo.cs
@@ -1,17 +1,21 @@
 ﻿using AegirLib.MathType;
+using AegirLib.Scene;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
 
 namespace Aegir.Rendering
 {
     public interface IGizmo
     {
         Vector3 Position { get; }
-        Quaternion Rotation { get; }
+        AegirLib.MathType.Quaternion Rotation { get; }
 
+        Visual3D Visual { get; }
 
+        bool SelectedSceneNodeChanged(Entity entity);
     }
 }
---
+++ Aegir/Rendering/Gizmo/TranslateGizmo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+using AegirLib.MathType;
+using AegirLib.Scene;
+
+namespace Aegir.Rendering.Gizmo
+{
+    public class TranslateGizmo : IGizmo
+    {
+        public Vector3 Position
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public AegirLib.MathType.Quaternion Rotation
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Visual3D Visual
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool SelectedSceneNodeChanged(Entity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
